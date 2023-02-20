b75f884b98468beb715ea9befc9fb17bcaf96d46
zqxul <xuzhiqing2014@outlook.com>
2023-02-20 13:34:25 +0800

    fix

--- main.go
+++ main.go
@@ -5,25 +5,7 @@ import (
 
 	"gate.io/channel"
 	"gate.io/job"
-	"github.com/gateio/gateapi-go/v6"
 )
-
-var client *gateapi.APIClient
-
-var secondClient *gateapi.APIClient
-
-func init() {
-	cfg := gateapi.NewConfiguration()
-	cfg.Key = channel.Key
-	cfg.Secret = channel.Secret
-	client = gateapi.NewAPIClient(cfg)
-	// client.ChangeBasePath("https://fx-api-testnet.gateio.ws/api/v4")
-
-	secondCfg := gateapi.NewConfiguration()
-	secondCfg.Key = channel.SecondKey
-	secondCfg.Secret = channel.SecondSecret
-	secondClient = gateapi.NewAPIClient(secondCfg)
-}
 
 func main() {
 	// go job.NewSpotJob(channel.CurrencyPairDOGE_USDT, 50, channel.Key, channel.Secret).Start(context.TODO())
@@ -33,6 +15,7 @@ func main() {
 	// go job.NewSpotJob(channel.CurrencyPairBSW_USDT, 50, channel.Key, channel.Secret).Start(context.TODO())
 	// go job.NewSpotJob(channel.CurrencyPairVGX_USDT, 50, channel.Key, channel.Secret).Start(context.TODO())
 	go job.NewSpotJob(channel.CurrencyPairCORE_USDT, 50, channel.Key, channel.Secret).Start(context.TODO())
+
 	go job.NewSpotJob(channel.CurrencyPairMAPE_USDT, 15, channel.SecondKey, channel.SecondSecret).Start(context.TODO())
 	go job.NewSpotJob(channel.CurrencyPairVGX_USDT, 65, channel.SecondKey, channel.SecondSecret).Start(context.TODO())
 	go job.NewSpotJob(channel.CurrencyPairBABY_USDT, 50, channel.SecondKey, channel.SecondKey).Start(context.TODO())
