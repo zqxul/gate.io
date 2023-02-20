7d68b2c52f51f756947b770924de8a52d918cbd1
zqxul <xuzhiqing2014@outlook.com>
2023-02-20 22:35:28 +0800

    fix

--- job/job.go
+++ job/job.go
@@ -263,6 +263,7 @@ func (job *SpotJob) handleOrderPutEvent(ctx context.Context, order *channel.Orde
 }
 
 func (job *SpotJob) refreshOrders(ctx context.Context) {
+	time.Sleep(time.Second)
 	job.mux.Lock()
 	defer job.mux.Unlock()
 
