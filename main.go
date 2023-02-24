6095f5542c4f19fe119921aa82d0b6f84d2814e4
zqxul <xuzhiqing2014@outlook.com>
2023-02-24 20:27:57 +0800

    FIX

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ func main() {
 	// go job.NewSpotJob(channel.CurrencyPairCORE_USDT, 50, 0.003, channel.Key, channel.Secret).Start(context.TODO())
 
 	// go job.NewSpotJob(channel.CurrencyPairMAPE_USDT, decimal.NewFromFloat(65), decimal.NewFromFloat(0.002), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairVGX_USDT, decimal.NewFromFloat(65), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
+	// go job.New(channel.CurrencyPairVGX_USDT, decimal.NewFromFloat(65), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBABY_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBLUR_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairAVT_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
+	go job.New(channel.CurrencyPairAVT_USDT, decimal.NewFromFloat(100), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBSW_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairCORE_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairXRP_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.0015), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairDOGE_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
+	// go job.New(channel.CurrencyPairCORE_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
+	go job.New(channel.CurrencyPairXRP_USDT, decimal.NewFromFloat(80), decimal.NewFromFloat(0.0015), channel.SecondKey, channel.SecondSecret).Start()
+	go job.New(channel.CurrencyPairDOGE_USDT, decimal.NewFromFloat(100), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 
 	api.Run()
 	select {}
