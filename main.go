acc909d46eb596ce9a16900d93b705c2c60f33fc
zqxul <xuzhiqing2014@outlook.com>
2023-02-24 20:29:54 +0800

    FIX

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// go job.New(channel.CurrencyPairVGX_USDT, decimal.NewFromFloat(65), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBABY_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBLUR_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
-	go job.New(channel.CurrencyPairAVT_USDT, decimal.NewFromFloat(100), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
+	// go job.New(channel.CurrencyPairAVT_USDT, decimal.NewFromFloat(100), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairBSW_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	// go job.New(channel.CurrencyPairCORE_USDT, decimal.NewFromFloat(50), decimal.NewFromFloat(0.003), channel.SecondKey, channel.SecondSecret).Start()
 	go job.New(channel.CurrencyPairXRP_USDT, decimal.NewFromFloat(80), decimal.NewFromFloat(0.0015), channel.SecondKey, channel.SecondSecret).Start()
